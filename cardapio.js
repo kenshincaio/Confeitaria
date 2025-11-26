15d5720593e3babe8d2a59f5ce3b2895467dd91c
Caio Yamada <yamada.caiokenshin@gmail.com>
2025-11-26 20:00:42 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,17 @@
 alert('oi foi carregado')
+// Vamos criar os elementos do HTML com JavaScript dinamicamente
+
+function criarItemCardapio(titulo, descricao, foto){
+  const divItemCardapio = document.createElement('div') 
+  // colocar a classe
+  
+  const h3Titulo = document.createElement('h3')
+  h3Titulo.textContent = titulo
+
+  const pDescricao = document.createElement('p')
+  pDescricao.textContent = descricao
+  // adicionar a classe
+
+  const img = document.createElement('img')
+  img.src =  foto
+}
