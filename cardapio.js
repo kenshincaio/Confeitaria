7d1bc4787f9b605adb647119874d822de647c621
Caio Yamada <yamada.caiokenshin@gmail.com>
2025-11-28 19:20:54 -0300

    Refactor criarItemCardapio function for clarity

--- cardapio.js
+++ cardapio.js
@@ -1,32 +1,35 @@
-// Vamos criar os elementos do HTML com JavaScript dinamicamente
+// Vamos criar os elementos
+// do HTML com JavaScript
+// Dinamicamente
 
-function criarItemCardapio(titulo, descricao, foto){
-  const divItemCardapio = document.createElement('div') 
-  divItemCardapio.className = 'item-cardapio'
-  // colocar a classe
-  
-  const h3Titulo = document.createElement('h3')
-  h3Titulo.textContent = titulo
+function criarItemCardapio(titulo, descricao, foto) {
+    const divItemCardapio = document.createElement('div')
+    divItemCardapio.className = 'item-cardapio'
 
-  const pDescricao = document.createElement('p')
-  pDescricao.textContent = descricao
-  pDescricao.className = 'descricao'
-  // adicionar a classe
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
 
-  const img = document.createElement('img')
-  img.src =  foto
-  img.className = 'img-item'
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao
+    pDescricao.className = 'descricao'
 
-  // Adicionando os elementos na div Mãe
-  const divC = document.getElementById
+    const img = document.createElement('img')
+    img.src = foto
+    img.className = 'img-item'
 
-  divItemCardapio.appendChild(h3Titulo)
-  divItemCardapio.appendChild(pDescricao)
-  divItemCardapio.appendChild(img)
+    // Adicionando os elementos na div Mãe
+    const divC = document.getElementById('cardapio')
 
-  divC.appendChild(divItemCardapio)
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio)
 }
 
-// executando a function
-
-criarItemCardapio('Bolo de Chocolate', 'Um clássico irresistível com camadas de chocolate', 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg')
+// Executando a função
+criarItemCardapio(
+    'Bolo de Chocolate',
+    'Um clássico irresistível com camadas de chocolate',
+    'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg'
+)
